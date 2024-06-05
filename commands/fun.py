e827bd4255e7cf8ec557ca345a715b23643ec3ee
Deadly <info.saddiki@gmail.com>
2024-06-05 10:26:39 +0200

    Rewrite of commands.fun.cool_command output

--- commands/fun.py
+++ commands/fun.py
@@ -18,7 +18,7 @@ class FunCommands(Extension):
         SlashCommandOption("n2", OptionType.INTEGER, "Second integer", required=True)
     ])
     async def add_command(self, ctx: SlashContext, **kwargs):
-        await ctx.send(f":ok_hand: `{ctx.kwargs["n1"]}` and `{ctx.kwargs["n2"]}` is "
+        await ctx.send(f":ok_hand: `{ctx.kwargs["n1"]}` + `{ctx.kwargs["n2"]}` is "
                        f"`{ctx.kwargs["n1"] + ctx.kwargs["n2"]}`.")
 
     # Cool command
@@ -26,10 +26,15 @@ class FunCommands(Extension):
         SlashCommandOption("target", OptionType.USER, "Target user", required=True)
     ])
     async def cool_command(self, ctx: SlashContext, **kwargs):
-        if randint(0, 10) <= 3:
-            await ctx.send(f":sunglasses: `{ctx.kwargs["target"].mention}` is very cool!")
-        else:
-            await ctx.send(f":clown: {ctx.kwargs['target'].mention} is not cool, but a clown.")
+        match randint(0, 10) <= 3:
+            case n if n == 1:
+                await ctx.send(f":star_struck: {ctx.kwargs["target"].mention} is the most amazing person EVAR!.")
+            case n if n <= 3:
+                await ctx.send(f":sunglasses: {ctx.kwargs["target"].mention} is very cool!")
+            case n if n <= 5:
+                await ctx.send(f":smiley: {ctx.kwargs["target"].mention} is okay.")
+            case _:
+                await ctx.send(f":clown: {ctx.kwargs['target'].mention} is not cool, but a clown.")
 
     # Joined command
     @slash_command("joined", description="Check a person's join date", options=[
